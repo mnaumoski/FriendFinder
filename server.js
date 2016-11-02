2702ce9936494237c71bc52ff41489712a261cbd
mnaumoski <naumoski.marija@gmail.com>
2016-11-02 13:18:47 -0400

    server.js setup

--- server.js
+++ server.js
@@ -17,9 +17,27 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // ===============================================================
-
+// Standard code for bodyParser
 // ===============================================================
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+
+// ===============================================================
+// Router
+//The below points our server to a series of "route" files
+//The route files give the server a map of how to respons when users visit or request data from different URLs.
+// ===============================================================
+
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+// ===============================================================
+// Listener: starting the server
+// ===============================================================
+
+app.listen(PORT, function(){
+      console.log("App listening on port: " + PORT)
+});
